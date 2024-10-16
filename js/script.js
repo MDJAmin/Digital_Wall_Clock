87717b1d7b96138f9aa8d1b9892fc03b0d8e58d8
MDJAmin <mohammadamindf@gmail.com>
2024-10-17 00:02:37 +0330

    Refactor : Complete Functionality in JS

--- js/script.js
+++ js/script.js
@@ -1,26 +1,46 @@
 "use strict"
 
-const numberHours = document.querySelector(".number-hours")
-const barSeconds = document.querySelector(".bar-second")
+setInterval(getCurrentTime, 1000);
+
+const numberHours = document.querySelector('.number-hours');
+const barSeconds = document.querySelector('.bar-seconds');
 
 const numberElement = [];
 const barElement = [];
 
-for(let i = 1; i <=12; i++){
-    numberElement.push(
-        `<span style="--index:${i};"><p>${i}</p></span>`
+
+for (let i = 1; i <= 12; i++) {
+  numberElement.push(
+    `<span style="--index:${i};"><p>${i}</p></span>`
     );
 }
 
-numberHours.insertAdjacentHTML("afterbegin", numberElement.
-    join(""));
 
-    
+numberHours.insertAdjacentHTML("afterbegin", numberElement.
+join(""));
+
 for (let i = 1; i <= 60; i++) {
-    barElement.push(
-      `<span style="--index:${i};"><p></p></span>`
-      );
-  }
-  
-  barSeconds.insertAdjacentHTML("afterbegin",barElement.
-  join(""));
+  barElement.push(
+    `<span style="--index:${i};"><p></p></span>`
+    );
+}
+
+barSeconds.insertAdjacentHTML("afterbegin",barElement.
+join(""));
+
+const handHours = document.querySelector('.hand.hours');
+const handMinutes = document.querySelector('.hand.minutes');
+const handSeconds = document.querySelector('.hand.seconds');
+
+function getCurrentTime() {
+  let date = new Date( );
+  let CurrentHours = date.getHours();
+  let CurrentMinutes = date.getMinutes();
+  let Currentseconds = date.getSeconds();
+
+  handHours.style.transform = `rotate(${CurrentHours * 30 + CurrentMinutes / 2 }deg)`;
+  handMinutes.style.transform = `rotate(${CurrentMinutes * 6}deg)`;
+  handSeconds.style.transform = `rotate(${Currentseconds * 6}deg)`;
+}
+getCurrentTime();
+
