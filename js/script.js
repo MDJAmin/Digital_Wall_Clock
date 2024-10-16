ae8f035645ad0af6697c48aed4e3f296bf3d182f
MDJAmin <mohammadamindf@gmail.com>
2024-10-16 23:54:37 +0330

    Add some EL from Dom and loop them

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 "use strict"
+
+const numberHours = document.querySelector(".number-hours")
+const barSeconds = document.querySelector(".bar-second")
+
+const numberElement = [];
+const barElement = [];
+
+for(let i = 1; i <=12; i++){
+    numberElement.push(
+        `<span style="--index:${i};"><p>${i}</p></span>`
+    );
+}
+
+numberHours.insertAdjacentHTML("afterbegin", numberElement.
+    join(""));
+
+    
+for (let i = 1; i <= 60; i++) {
+    barElement.push(
+      `<span style="--index:${i};"><p></p></span>`
+      );
+  }
+  
+  barSeconds.insertAdjacentHTML("afterbegin",barElement.
+  join(""));
